3cd94b3b06744103f249b91a2c10f1a7b5178a09
Marilène <67200994+MarileneK@users.noreply.github.com>
2022-09-11 22:07:01 +0200

    fix | Replacing BrowserRoute by HashRouter

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {
-  BrowserRouter,
+  // BrowserRouter,
   Routes,
   Route,
+  HashRouter,
 } from "react-router-dom";
 import App from './App';
 import About from './pages/About';
@@ -13,13 +14,13 @@ import Projects from './pages/Projects/Projects';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path='/' element={<App />}/>
       <Route path='projects' element={<Projects />}/>
       <Route path='about' element={<About />}/>
       <Route path='contact' element={<Contact />}/>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
